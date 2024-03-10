6180f36bb8b774ff2b775530de20877658773f48
Roman Gonzalez <open-source@roman-gonzalez.info>
2024-03-10 14:59:46 -0700

    build: update to rust-1.76

--- nix/packages/toolchain.nix
+++ nix/packages/toolchain.nix
@@ -2,5 +2,5 @@
 
 fenix.packages.${system}.fromToolchainFile {
   file = "${self}/rust-toolchain.toml";
-  sha256 = "sha256-ks0nMEGGXKrHnfv4Fku+vhQ7gx76ruv6Ij4fKZR3l78=";
+  sha256 = "sha256-e4mlaJehWBymYxJGgnbuCObVlqMlQSilZ8FljG9zPHY=";
 }
